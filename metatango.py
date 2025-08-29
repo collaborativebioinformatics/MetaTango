6e022cc8d4f6991a77de28d8dca285ea68f6dfa6
Ryan Doughty <rdd4@p00.cs.rice.edu>
2025-08-28 20:29:57 -0500

    reorganizing a bit

---
+++ metatango.py
@@ -0,0 +1,8 @@
+import argparse
+
+def parse_args():
+    
+    parser = argparse.ArgumentParser(description='MetaTango: comparing reference and graph based SV detection in microbiomes')
+
+if __name__=="__main__":
+    parse_args()
